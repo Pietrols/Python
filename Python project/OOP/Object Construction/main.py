2da22c5709e50c4c3bfad11337150b1a0bcd3cec
Peter Kabamba <kabambapeter3@gmail.com>
2026-02-24 01:21:33 +0200

    object constructor practice

---
+++ Python project/OOP/Object Construction/main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+# object = class (object construction)
+timmy = Turtle()
+print(timmy)
+timmy.shape('turtle')
+timmy.color('red')
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
